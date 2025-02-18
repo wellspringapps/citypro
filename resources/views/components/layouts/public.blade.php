e5f85d9b4ca91ea2b50ce2d71340646d34565613
Darrin Deal <darrin.deal@icloud.com>
2025-02-18 13:35:57 -0500

    Canonical

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -11,6 +11,8 @@
             <meta name="description" content="{{ $description }}" />
         @endif
 
+        <link rel="canonical" href="{{ request()->route()->uri() }}" />
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
